e4626a49081d9b617f36d73c763d78e437b5d8cf
tryan19 <tryan19@gmail.com>
2017-11-14 11:33:21 -0500

    Face

--- chapter04/face.py
+++ chapter04/face.py
@@ -4,12 +4,60 @@ from graphics import *
 def main():
     win = GraphWin("Face Drawing", 400, 400)
 
-    # Here's where I will draw the face!
 
-    win.getMouse()
+    # Draw your face here!
+
+    center = Point(100, 100)
+    circle = Circle(center, 30)
+    circle.setFill('blue')
+    circle.draw(win)
+    label = Text(center, "")
+    label.draw(win)
+
+    center= Point(100,100)
+    circle = Circle(center, 20)
+    circle.setFill('green')
+    circle.draw(win)
+    label = Text(center, "")
+    label.draw(win)
+
+    center = Point(200, 100)
+    circle = Circle(center, 30)
+    circle.setFill('red')
+    circle.draw(win)
+    label = Text(center, "")
+    label.draw(win)
+
+    center = Point(200, 100)
+    circle = Circle(center, 20)
+    circle.setFill('green')
+    circle.draw(win)
+    label = Text(center, "")
+    label.draw(win)
+
+    center = Point(200, 100)
+    circle = Circle(center, 5)
+    circle.setFill(color_rgb(0,0,0))
+    circle.draw(win)
+    label = Text(center, "")
+    label.draw(win)
+
+    center = Point(100, 100)
+    circle = Circle(center, 5)
+    circle.setFill(color_rgb(0,0,0))
+    circle.draw(win)
+    label = Text(center, "")
+    label.draw(win)
+
+
+
+
+
+
+
+    input("Press any key to quit")
 
 
 main()
-
 
 
