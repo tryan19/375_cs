9a29542664f7aaa58b95dcbc3c2580de0f62f43f
tryan19 <tryan19@gmail.com>
2017-11-28 11:14:14 -0500

    final house

--- chapter04/picture.py
+++ chapter04/picture.py
@@ -4,13 +4,12 @@ from graphics import *
 def main():
     win = GraphWin("Face Drawing", 400, 400)
 
-
     # Draw your face here!
     size = eval(input("How big do you want to make the window? "))
     win = GraphWin("Scaling Example", size, size)
     win.setCoords(0, 0, 10, 10)
 
-    #outline of house
+    # outline of house
     rectangle = Rectangle(Point(2.1, 1), Point(2, 8))
     rectangle.setFill('yellow')
     rectangle.setOutline('black')
@@ -31,13 +30,13 @@ def main():
     rectangle.setOutline('black')
     rectangle.draw(win)
 
-    #sun
+    # sun
     center = Point(.5, 9.2)
     circ = Circle(center, 1.1)
     circ.setFill(color_rgb(255,165,0))
     circ.draw(win)
 
-    #Door frame
+    # Door frame
     line = Line(Point(3, 1.1), Point(3, 3))
     line.draw(win)
 
@@ -48,19 +47,19 @@ def main():
     line.draw(win)
 
 
-    #Door knob
+    # Door knob
     center = Point(3.2, 2)
     circ = Circle(center, .1)
     circ.setFill(color_rgb(0,0,0))
     circ.draw(win)
 
-    #Grass
+    # Grass
     rectangle = Rectangle(Point(0, 0), Point(10, 1))
     rectangle.setFill(color_rgb(34,139,34))
     rectangle.setOutline(color_rgb(34,139,34))
     rectangle.draw(win)
 
-    #Roof
+    # Roof
     polygon = Polygon (Point(2, 8.1) , Point(5, 10)  , Point(8.1,8.1))
     polygon.draw(win)
     polygon.setFill('blue')
