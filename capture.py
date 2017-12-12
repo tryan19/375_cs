dca3654668e6962718b40a22907556165b5e58b0
tryan19 <tryan19@gmail.com>
2017-12-12 11:29:15 -0500

    capture.py

--- capture.py
+++ capture.py
@@ -12,6 +12,7 @@ from math import *
 def main():
     print("Press the arrow keys to move the white circle, press 'q' to quit")
     score = 0
+    timer = 60
 
     window_size = 600
     canvas_size = 20
@@ -21,8 +22,17 @@ def main():
     win.setBackground("black")
 
     score_text = Text(Point(2,18),"Score:0")
-    score_text.setTextcolor("white")
+    score_text.setFill("white")
     score_text.draw(win)
+    score_text.setFace("courier")
+    score_text.setSize(25)
+
+    time_text = Text(Point(18,18),"Timer:60")
+    time_text.setFill("white")
+    time_text.draw(win)
+    time_text.setFace("courier")
+    time_text.setSize(25)
+
 
     circle = Circle(Point(canvas_size / 2, canvas_size / 2), 2)
     circle.setFill("white")
@@ -33,8 +43,17 @@ def main():
     pellet.draw(win)
     move_to_random_point(pellet, canvas_size)
 
-    while True:
-        key = win.getKey()
+    time_check = time.time()
+
+    message_text = Text (Point(canvas_size/2, canvas_size /2), "CLICK TO START")
+    message_text.setTextColor("red")
+    message_text.draw(win)
+    message_text.setFace("courier")
+    message_text.setSize(30)
+    message_text.setText("")
+
+    while timer > 0:
+        key = win.checkKey()
 
         delta_x = 0
         delta_y = 0
@@ -55,8 +74,11 @@ def main():
         if collide(circle, pellet):
             move_to_random_point(pellet, canvas_size)
             score += 1
-            print (score)
-
+            score_text.setText("score:" + str(score))
+        if time.time() - time_check > 1 :
+            timer -= 1
+            time_check = time.time()
+            time_text.setText("Time:" + str(timer))
 
 def collide(c1, c2):
     a = c1.getCenter().getX() - c2.getCenter().getX()
