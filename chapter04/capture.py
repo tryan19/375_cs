11c76eb65a99a87baca7af69de75493fe3153e48
tryan19 <tryan19@gmail.com>
2017-12-22 11:16:32 -0500

    capture.py my changes final

--- chapter04/capture.py
+++ chapter04/capture.py
@@ -1,10 +1,11 @@
 # capture_the_pellets.py
 #
 # This program is a simple game called "Capture the Pellets!". The user moves a large white ball around the screen
-# using the arrow keys to "capture" a small, randomly-placed, yellow pellet. The program keeps track of the score and a
-# countdown timer. The goal is to capture as many pellets as possible before time expires.
+# using the arrow keys to "capture" a small, randomly-placed, yellow pellet. They also can get a blue pellet that
+# is worth five points and is also on the screen.  The program keeps track of the score and a
+# countdown timer it also keeps track of the high score. The goal is to capture as many pellets as possible before time expires.
 #
-# by Mr. Ciccolo
+# by Tom Ryan
 
 from graphics import *
 from math import *
@@ -17,27 +18,36 @@ def main():
     score = 0
     timer = 60
 
+    file = open("chapter04/highscore.txt", "r")
+    highscore = eval(file.read())
+
     window_size = 600
     canvas_size = 20
 
     win = GraphWin("Capture the Pellets!", window_size, window_size)
     win.setCoords(0, 0, canvas_size, canvas_size)
-    win.setBackground("black")
+    win.setBackground("pink")
 
     score_text = Text(Point(0.2 * canvas_size, 0.9 * canvas_size), "Score: " + str(score))
-    score_text.setTextColor("white")
+    score_text.setTextColor("black")
     score_text.setFace("courier")
     score_text.setSize(24)
     score_text.draw(win)
 
+    Highscore_text = Text(Point(0.5 * canvas_size, 0.9 * canvas_size), ("High score: " + str(highscore)))
+    Highscore_text.setTextColor("black")
+    Highscore_text.setFace("courier")
+    Highscore_text.setSize(24)
+    Highscore_text.draw(win)
+
     timer_text = Text(Point(0.8 * canvas_size, 0.9 * canvas_size), "Time: " + str(timer))
-    timer_text.setTextColor("white")
+    timer_text.setTextColor("black")
     timer_text.setFace("courier")
     timer_text.setSize(24)
     timer_text.draw(win)
 
     message_text = Text(Point(canvas_size / 2, canvas_size / 2), "CLICK TO START")
-    message_text.setTextColor("green")
+    message_text.setTextColor("blue")
     message_text.setFace("courier")
     message_text.setSize(36)
     message_text.draw(win)
@@ -86,17 +96,29 @@ def main():
             move_to_random_point(pellet, canvas_size)
 
         if collide(ball, special_pellet):
-            score += 5
-            score_text.setText("score:" + str(score))
+            score +=5
+            score_text.setText("Score:" + str(score))
             move_to_random_point(special_pellet, canvas_size)
+
+        if score > highscore:
+            highscore = score
+            Highscore_text.setText("High score: " + str(highscore))
 
         if (time.time() - time_check) > 1:
             time_check = time.time()
             timer -= 1
             timer_text.setText("Time: " + str(timer))
 
-    message_text.setTextColor("red")
+
+
+
+
+    message_text.setTextColor("blue")
     message_text.setText("GAME OVER")
+
+    file = open("chapter04/highscore.txt", "w")
+    file.write(str(highscore))
+    file.close()
 
     input("Game over! Your final score was " + str(score) + ". Press Enter to quit.")
 
